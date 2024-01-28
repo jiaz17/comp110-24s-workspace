2a57367950cc15e2b73167f2c80f88ebff7a8863
Julia Zhu <jiaz@unc.edu>
2024-01-27 21:24:08 -0500

    final battership

--- exercises 2/ex01_simple_battleship.py
+++ exercises 2/ex01_simple_battleship.py
@@ -1,18 +1,16 @@
-"""EX01 - Simple Battleship - A cute step toward Battleship."""
+"""EX01 - Simple Battleship - A cute step toward Battleship"""
 
 __author__ = "730679888"
 BLUE_BOX = "\U0001F7E6"
 RED_BOX = "\U0001F7E5"
-WHITE_BOX = "\U00002B1C"
+WHITE_BOX = "\U0001F7EB"
 
 boat_location = int(input("Pick a secret boat location between 1 and 4: "))
 if boat_location < 1:
     print("Error! 0 too low!")
-    exit()
 elif boat_location > 4:
     print("Error! 5 too high!")
-    exit()
-
+full_emoji_string = BLUE_BOX * 4
 guess = int(input("Guess a number between 1 and 4: "))
 if guess < 1:
     print("Error! 0 too low!")
@@ -28,6 +26,5 @@ else:
     print("Incorrect! You missed the ship.")
     result = WHITE_BOX
 
-full_emoji_string = BLUE_BOX * 4
-full_emoji_string = full_emoji_string[:guess - 1] + result + full_emoji_string[guess:]
+full_emoji_string = full_emoji_string[:guess-1] + result + full_emoji_string[guess:]
 print(full_emoji_string)
