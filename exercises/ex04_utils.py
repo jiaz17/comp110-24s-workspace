8dbfabbd7ceb117fc7ba723079db912f8b87afad
Julia Zhu <jiaz@unc.edu>
2024-02-25 18:44:48 -0500

    ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,7 +1,8 @@
-"""Ex04_Utils.py"""
+"""Ex04_Utils."""
 __author__ = "730679888"
 
-def all(int_list, single_int):
+
+def all(int_list: list[int], single_int: int) -> bool:
     """Bool test indicated number."""
     if len(int_list) == 0:
         return False
@@ -11,8 +12,8 @@ def all(int_list, single_int):
     return True
 
 
-def max(input_list):
-    """Return the largest number in the list. """
+def max(input_list: list[int]) -> int:
+    """Return the largest number in the list."""
     if len(input_list) == 0:
         raise ValueError("max() arg is an empty List")
     largest = input_list[0]
@@ -23,7 +24,7 @@ def max(input_list):
     return largest
 
 
-def is_equal(list1, list2):
+def is_equal(list1: list[int], list2: list[int]) -> bool:
     """Determination if every element at every index is equal in both lists."""
     if len(list1) != len(list2):
         return False
@@ -33,7 +34,7 @@ def is_equal(list1, list2):
     return True
 
 
-def extend(list1, list2):
-    """Appending values to first list. """
+def extend(list1: list[int], list2: list[int]) -> None:
+    """Appending values to first list."""
     for element in list2:
         list1.append(element)
