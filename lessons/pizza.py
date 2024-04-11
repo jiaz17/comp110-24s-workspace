d695fc4d3f183ef9e00ca2e36d8ffb044af35b5f
Julia Zhu <jiaz@ad.unc.edu>
2024-04-11 19:53:13 -0400

    4/11

---
+++ lessons/pizza.py
@@ -0,0 +1,15 @@
+"Define Pizza class"
+#arrtibutes:
+size : str
+toppings: int
+gluten_free: bool
+
+def __init__(self, size_input: str, topping_input: int, gluten_free_input:bool):
+    self.size = size_input
+    self.toppings = topping_input
+    self.gluten_free = gluten_free_input
+    #it actually returns self
+
+
+
+    
