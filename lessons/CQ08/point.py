cada6c4144e9c3bea2ee2f56f702a1be6847e77f
Julia Zhu <jiaz@ad.unc.edu>
2024-04-12 20:59:02 -0400

    4/12

---
+++ lessons/CQ08/point.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x_init: float, y_init: float):
+        self.x = x_init
+        self.y = y_init
+        
+    def scale_by(self, factor: int):
+        """ Mutating method"""
+        self.x *= factor
+        self.y *= factor
+        
+    def scale(self, factor: int) -> 'Point':
+        """Non-mutating method that returns a new Point object."""
+        return Point(self.x * factor, self.y * factor)
