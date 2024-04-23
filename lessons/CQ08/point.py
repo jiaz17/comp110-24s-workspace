7c58f969bf13f477c9e6fa3017162be1234af2a7
Julia Zhu <jiaz@ad.unc.edu>
2024-04-22 21:21:43 -0400

    42/2

--- lessons/CQ08/point.py
+++ lessons/CQ08/point.py
@@ -1,15 +1,23 @@
+"""CQ08 - Intro to Object Oriented Programming."""
+from __future__ import annotations
+__author__ = "730679888"
+
+
 class Point:
-    def __init__(self, x_init: float, y_init: float):
+    """Class Point has x,y attribute."""
+    x: float
+    y: float
+
+    def __init__(self, x_init: float, y_init: float) -> None:
+        """Point Class."""
         self.x = x_init
         self.y = y_init
-        
-        
-    def scale_by(self, factor: int):
-        """ Mutating method"""
+
+    def scale_by(self, factor: int) -> None:
+        """Mutating method."""
         self.x *= factor
         self.y *= factor
         
-
-    def scale(self, factor: int) -> 'Point':
+    def scale(self, factor: int) -> Point:
         """Non-mutating method that returns a new Point object."""
         return Point(self.x * factor, self.y * factor)
